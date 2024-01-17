6e288f1d1fe95e4f6e580edad32ea99b48df080c
MAHDI-EIVAZI <mahdieivazi.1397@gmail.com>
2024-01-17 14:30:38 +0330

    Setting Properties

--- obj/Debug/net8.0-windows/Tic-Tac-Toe.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Tic-Tac-Toe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tic-Tac-Toe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e381b1d9ce591f3ec8ff829f8d7786576dde10c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tic-Tac-Toe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tic-Tac-Toe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
