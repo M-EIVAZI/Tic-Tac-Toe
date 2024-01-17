e968255224bbd390ba5bd714a16b8e170ee0c7e9
MAHDI-EIVAZI <mahdieivazi.1397@gmail.com>
2024-01-17 16:18:06 +0330

    add is_win function

--- Form1.cs
+++ Form1.cs
@@ -5,9 +5,15 @@ namespace Tic_Tac_Toe
         public MainForm()
         {
             InitializeComponent();
+            Fill_Empty();
         }
         private char[,] cells = new char[3, 3];
+        private void Fill_Empty()
+        {   for (int i = 0;i<3;i++) 
+                for(int j = 0;j<3;j++)
+                    cells[i,j] = ' ';
 
+        }
 
         private void button1_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -148,7 +154,43 @@ namespace Tic_Tac_Toe
         {
 
         }
-        //private Is_full
+        private bool Is_win(Char player)
+        {
+            int count = 0;
+            for(int i=0;i<3;i++) 
+            {
+                if ((cells[i, 0] == player && cells[i, 1] == player && cells[i, 2] == player) || (cells[0, i] == player && cells[1, i] == player && cells[2, i] == player))
+                    return true;
+            }
+            if ((cells[0, 0] == player && cells[1, 1] == player && cells[2,2]==player) || (cells[0,2]==player && cells[1,1]==player && cells[2,0]==player))
+                return true;
+            return false;
+        }
+        private bool Is_full()
+        {
+            for(int i = 0; i < 3; i++) 
+            {
+                for(int j= 0; j < 3;j++)
+                {
+                    if (cells[i,j]==' ')
+                        return true;
+                }
+            }
+            return false;
+
+        }
+        private int? EvaluateBoard()
+        {   if (Is_win('X'))
+                return -1;
+            if (Is_win('O'))
+                return +1;
+            if (Is_full())
+                return 0;
+            return null;
+
+        }
+
+     
     }
 
 }
